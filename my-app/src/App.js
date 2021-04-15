bf77e0fb1b019e845057b24b6eb76f25da690c13
SVLakhtarev <svlakhtarev@gmail.com>
2021-04-15 14:44:42 +0300

    add directory

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'; 
+import Nav from './components/Nav'
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i2.wp.com/nopcproblem.ru/wp-content/uploads/2019/05/discord-logo.png' />
+        </header>
+    )
+}
+
+export default Header;
