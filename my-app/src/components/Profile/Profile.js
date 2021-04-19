4434245a701eed8ee07018275bac04180c39aa5c
SVLakhtarev <svlakhtarev@gmail.com>
2021-04-19 13:49:34 +0300

    new

--- my-app/src/components/Profile/Profile.js
+++ my-app/src/components/Profile/Profile.js
@@ -5,8 +5,8 @@ import s from './Profile.module.css';
 const Profile = () => {
     return (
     <div className={s.content}>
-      <div className={s.pImg}>
-         <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+      <div>
+         <img className={s.pImg} src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
       </div>
       <div>
         Avatar+ description
